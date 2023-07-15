2980ad84cd20b3a5d8ea37cc04b384c74f1738f9
ssshooter <ssshooterx@gmail.com>
2023-07-15 19:14:54 +0800

    chore: update dep

--- nodeMenu.ts
+++ nodeMenu.ts
@@ -1,6 +1,7 @@
 import './nodeMenu.less'
 import './icons/iconfont.js'
 import i18n from './i18n.js'
+import type { MindElixirInstance } from 'mind-elixir'
 
 const createDiv = (id, innerHTML) => {
   const div = document.createElement('div')
@@ -30,7 +31,7 @@ const colorList = [
   '#2ecc71',
 ]
 
-export default function (mind) {
+export default function (mind: MindElixirInstance) {
   console.log('install node menu')
   function clearSelect(klass, remove) {
     const elems = mind.container.querySelectorAll(klass)
@@ -46,7 +47,7 @@ export default function (mind) {
     `
   <div class="nm-fontsize-container">
     ${['15', '24', '32']
-      .map(size => {
+      .map((size) => {
         return `<div class="size"  data-size="${size}">
     <svg class="icon" style="width: ${size}px;height: ${size}px" aria-hidden="true">
       <use xlink:href="#icon-a"></use>
@@ -58,7 +59,7 @@ export default function (mind) {
   </div>
   <div class="nm-fontcolor-container">
     ${colorList
-      .map(color => {
+      .map((color) => {
         return `<div class="split6"><div class="palette" data-color="${color}" style="background-color: ${color};"></div></div>`
       })
       .join('')}
@@ -68,10 +69,24 @@ export default function (mind) {
   <span class="background">${i18n[locale].background}</span>
   </div>`
   )
-  const tagDiv = createDiv('nm-tag', `${i18n[locale].tag}<input class="nm-tag" tabindex="-1" placeholder="${i18n[locale].tagsSeparate}" />`)
-  const iconDiv = createDiv('nm-icon', `${i18n[locale].icon}<input class="nm-icon" tabindex="-1" placeholder="${i18n[locale].iconsSeparate}" />`)
-  const urlDiv = createDiv('nm-url', `${i18n[locale].url}<input class="nm-url" tabindex="-1" />`)
-  const memoDiv = createDiv('nm-memo', `${i18n[locale].memo || 'Memo'}<textarea class="nm-memo" rows="5" tabindex="-1" />`)
+  const tagDiv = createDiv(
+    'nm-tag',
+    `${i18n[locale].tag}<input class="nm-tag" tabindex="-1" placeholder="${i18n[locale].tagsSeparate}" />`
+  )
+  const iconDiv = createDiv(
+    'nm-icon',
+    `${i18n[locale].icon}<input class="nm-icon" tabindex="-1" placeholder="${i18n[locale].iconsSeparate}" />`
+  )
+  const urlDiv = createDiv(
+    'nm-url',
+    `${i18n[locale].url}<input class="nm-url" tabindex="-1" />`
+  )
+  const memoDiv = createDiv(
+    'nm-memo',
+    `${
+      i18n[locale].memo || 'Memo'
+    }<textarea class="nm-memo" rows="5" tabindex="-1" />`
+  )
 
   // create container
   const menuContainer = document.createElement('div')
@@ -92,7 +107,8 @@ export default function (mind) {
   // query input element
   const sizeSelector = menuContainer.querySelectorAll('.size')
   const bold: HTMLElement = menuContainer.querySelector('.bold')
-  const buttonContainer: HTMLElement = menuContainer.querySelector('.button-container')
+  const buttonContainer: HTMLElement =
+    menuContainer.querySelector('.button-container')
   const fontBtn: HTMLElement = menuContainer.querySelector('.font')
   const tagInput: HTMLInputElement = mind.container.querySelector('.nm-tag')
   const iconInput: HTMLInputElement = mind.container.querySelector('.nm-icon')
@@ -102,7 +118,7 @@ export default function (mind) {
   // handle input and button click
   let bgOrFont
   const E = mind.findEle
-  menuContainer.onclick = e => {
+  menuContainer.onclick = (e) => {
     if (!mind.currentNode) return
     const nodeObj = mind.currentNode.nodeObj
     const target = e.target as HTMLElement
@@ -124,7 +140,9 @@ export default function (mind) {
       target.className = 'background selected'
       target.previousElementSibling.className = 'font'
       if (nodeObj.style && nodeObj.style.background) {
-        menuContainer.querySelector('.palette[data-color="' + nodeObj.style.background + '"]').className = 'palette nmenu-selected'
+        menuContainer.querySelector(
+          '.palette[data-color="' + nodeObj.style.background + '"]'
+        ).className = 'palette nmenu-selected'
       }
     } else if (target.className === 'font') {
       clearSelect('.palette', 'nmenu-selected')
@@ -132,16 +150,20 @@ export default function (mind) {
       target.className = 'font selected'
       target.nextElementSibling.className = 'background'
       if (nodeObj.style && nodeObj.style.color) {
-        menuContainer.querySelector('.palette[data-color="' + nodeObj.style.color + '"]').className = 'palette nmenu-selected'
+        menuContainer.querySelector(
+          '.palette[data-color="' + nodeObj.style.color + '"]'
+        ).className = 'palette nmenu-selected'
       }
     }
   }
-  Array.from(sizeSelector).map(dom => {
-    ;(dom as HTMLElement).onclick = e => {
+  Array.from(sizeSelector).map((dom) => {
+    ;(dom as HTMLElement).onclick = (e) => {
       clearSelect('.size', 'size-selected')
       const size = e.currentTarget as HTMLElement
       size.className = 'size size-selected'
-      mind.reshapeNode(mind.currentNode, { style: { fontSize: size.dataset.size } })
+      mind.reshapeNode(mind.currentNode, {
+        style: { fontSize: size.dataset.size },
+      })
     }
   })
   bold.onclick = (e: MouseEvent & { currentTarget: Element }) => {
@@ -158,14 +180,16 @@ export default function (mind) {
     if (!mind.currentNode) return
     if (typeof e.target.value === 'string') {
       const newTags = e.target.value.split(',')
-      mind.reshapeNode(mind.currentNode, { tags: newTags.filter(tag => tag) })
+      mind.reshapeNode(mind.currentNode, { tags: newTags.filter((tag) => tag) })
     }
   }
   iconInput.onchange = (e: InputEvent & { target: HTMLInputElement }) => {
     if (!mind.currentNode) return
     if (typeof e.target.value === 'string') {
       const newIcons = e.target.value.split(',')
-      mind.reshapeNode(mind.currentNode, { icons: newIcons.filter(icon => icon) })
+      mind.reshapeNode(mind.currentNode, {
+        icons: newIcons.filter((icon) => icon),
+      })
     }
   }
   urlInput.onchange = (e: InputEvent & { target: HTMLInputElement }) => {
@@ -177,14 +201,14 @@ export default function (mind) {
     mind.currentNode.nodeObj.memo = e.target.value
     mind.bus.fire('operation', {
       name: 'updateMemo',
-      obj: mind.currentNode.nodeObj, 
+      obj: mind.currentNode.nodeObj,
     })
   }
   let state = 'open'
-  buttonContainer.onclick = e => {
+  buttonContainer.onclick = (e) => {
     menuContainer.classList.toggle('close')
     if (state === 'open') {
-      state = 'close' 
+      state = 'close'
       buttonContainer.innerHTML = `<svg class="icon" aria-hidden="true"><use xlink:href="#icon-menu"></use></svg>`
     } else {
       state = 'open'
@@ -207,13 +231,17 @@ export default function (mind) {
     fontBtn.nextElementSibling.className = 'background'
     if (nodeObj.style) {
       if (nodeObj.style.fontSize) {
-        menuContainer.querySelector('.size[data-size="' + nodeObj.style.fontSize + '"]').className = 'size size-selected'
+        menuContainer.querySelector(
+          '.size[data-size="' + nodeObj.style.fontSize + '"]'
+        ).className = 'size size-selected'
       }
       if (nodeObj.style.fontWeight) {
         menuContainer.querySelector('.bold').className = 'bold size-selected'
       }
       if (nodeObj.style.color) {
-        menuContainer.querySelector('.palette[data-color="' + nodeObj.style.color + '"]').className = 'palette nmenu-selected'
+        menuContainer.querySelector(
+          '.palette[data-color="' + nodeObj.style.color + '"]'
+        ).className = 'palette nmenu-selected'
       }
     }
     if (nodeObj.tags) {
